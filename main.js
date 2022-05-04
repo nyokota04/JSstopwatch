0a9f5bbcbce68a51bfff21e207a12b04a2b1aba6
nyokota04 <104046798+nyokota04@users.noreply.github.com>
2022-05-04 14:33:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,191 +1,107 @@
 
 
-
-// mSec-1
-let mS1 = 0 
-const mSec1= function mSec1(){
-let mSec1 = mS1;
-
-let count_show_mSec = document.getElementById("mSset1");
-function count_show(){
-  tmme1 = count_show_mSec.textContent = mSec1;
-};
-
-function mSec1_change(){
-    mSec1 ++;
-    if(mSec1 > 9){
-      mSec1 = 0;
-    }else{
-
-    }
-    count_show();
-}
-count_show();
-intervalid1 = setInterval(mSec1_change, 10);
-}
-
-// mSec-10
+let mS1 = 0;
 let mS10 = 0;
-const mSec10 = function mSec10(){
-  let mSec10 = mS10;
-  
-  let count_show_mSec = document.getElementById("mSset10");
-  function count_show(){
-      count_show_mSec.textContent = mSec10;
-  }
-  
-  function mSec10_change(){
-      mSec10 ++;
-      if(mSec10 > 9){
-        mSec10 = 0;
-      }
-      count_show();
-  }
-  count_show();
-  intervalid2 = setInterval(mSec10_change, 100);
-  }
+let sec1 = 0;
+let sec10 = 0;
+let mi1 = 0;
+let mi10 = 0;
+let ho = 0;
 
+function time_change(){
+    mS1 ++;
 
-// sec-1
-let se1 = 0;
-const sec1 = function sec1(){
-let sec1 = se1;
+    if(mS1 > 9){
+      mS1 = 0;
+      mS10 ++
+    }
 
-let count_show_sec = document.getElementById("secset1");
-function count_show(){
-    count_show_sec.textContent = sec1;
-}
-function sec1_change(){
-  sec1 ++;
-  if(sec1 > 9){
-    sec1 = 0;
-  }
-  count_show();
-}
-count_show();
-intervalid3 = setInterval(sec1_change, 1000);
+    if(mS10 > 9){
+      mS10 = 0;
+      sec1 ++
+    }
 
-}
+    if(sec1 > 9){
+      sec1 = 0;
+      sec10 ++
+    }
 
-// sec-10
-let se10 = 0;
-const sec10 = function sec10(){
-  let sec10 = se10;
-  
-  let count_show_sec = document.getElementById("secset10");
-  function count_show(){
-      count_show_sec.textContent = sec10;
-  }
-  function sec10_change(){
-    sec10 ++;
     if(sec10 > 5){
       sec10 = 0;
+      mi1 ++
     }
-    count_show();
-  }
-  count_show();
-  intervalid4 =  setInterval(sec10_change, 10000);
-  }
 
-// min-1
-let mi1 = 0;
-const min1 = function min1(){
-  let min1 = mi1;
-  
-  let count_show_sec = document.getElementById("minset1");
-  function count_show(){
-      count_show_sec.textContent = min1;
-  }
-  function min_change(){
-    min1 ++;
-    if(min1 > 9){
-      min1 = 0;
+    if(mi1 > 9){
+      mi1 = 0;
+      mi10 ++
     }
-    count_show();
-  }
-  count_show();
-  intervalid5 =  setInterval(min_change, 60000);
-  }
 
-// min-10
-let mi10 = 0;
-const min10 = function min10(){
-  let min10 = mi10;
-  
-  let count_show_sec = document.getElementById("minset10");
-  function count_show(){
-      count_show_sec.textContent = min10;
-  }
-  function min_change(){
-    min10 ++;
-    if(min10 > 5){
-      min10 = 0;
+    if(mi10 > 5){
+      mi10 = 0;
+      ho ++
     }
-    count_show();
-  }
-  count_show();
-  intervalid6 =  setInterval(min_change, 600000);
-  }
-
-// hour
-let ho = 0;
-const hour1 = function hour1(){
-  let hour1 = ho;
-  if(hour1 > 24){
-    hour1 = 0;
-  }
   
-  let count_show_sec = document.getElementById("hour");
-  function count_show(){
-      count_show_sec.textContent = hour1;
-  }
-  function hour_change(){
-    hour1 ++;
-    count_show();
-  }
-  count_show();
-  intervalid7 = setInterval(hour_change, 3600000);
-  }
 
+    let mSset1 = document.getElementById("mSset1");
+     mSset1.textContent = mS1;
+
+    let mSset10 = document.getElementById("mSset10");
+      mSset10.textContent = mS10;
+
+    let secset1 = document.getElementById("secset1");
+     secset1.textContent = sec1; 
+
+    let secset10 = document.getElementById("secset10");
+     secset10.textContent = sec10; 
+
+    let minset1 = document.getElementById("minset1");
+    minset1.textContent = mi1; 
+
+    let minset10 = document.getElementById("minset10");
+    minset10.textContent = mi10; 
+
+    let hourset = document.getElementById("hourset");
+    hourset.textContent = ho; 
+
+    msec1 = mS1;
+    msec10 = mS10;
+    seconds1 = sec1;
+    seconds10 = sec10;
+    minutes1 = mi1;
+    minutes10 = mi10;
+    hour = ho;
+    }
+
+
+
+
+// スタート
 
 function eventStart() {
-  mSec1();
-  mSec10();
-  sec1();
-  sec10();
-  min1();
-  min10();
-  hour1();
+  intervalid1 = setInterval(time_change, 10);
 };
 
-
+// ストップ
 function eventStop(){
   clearInterval(intervalid1);
-  clearInterval(intervalid2);
-  clearInterval(intervalid3);
-  clearInterval(intervalid4);
-  clearInterval(intervalid5);
-  clearInterval(intervalid6);
-  clearInterval(intervalid7);
 };
 
 // リセット
 function eventReset(){
-  // mS1 = 0;
+
   mSset1.innerHTML = 0;
   mS1 = 0;
+  mS10 = 0;
+  sec1 = 0;
+  sec10 = 0;
+  mi1 = 0;
+  mi10 = 0;
+  ho = 0;
+
   mSset10.innerHTML = 0;
-  mS1 = 0;
   secset1.innerHTML = 0;
-  mS1 = 0;
   secset10.innerHTML = 0;
-  mS1 = 0;
   minset1.innerHTML = 0;
-  mS1 = 0;
   minset10.innerHTML = 0;
-  mS1 = 0;
   hourset.innerHTML = 0;
 }
-
-const button = document.querySelector("eventStart");
-
